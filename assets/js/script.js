831052e3b5c1dbe8be15b8b0e98d86dd611d9ed5
dedelaravel15 <latihanlaravel15@gmail.com>
2023-11-17 14:47:55 +0700

    selesai

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,28 @@ menuToggle.addEventListener('click', () => {
     navMenu.classList.toggle("active");
 })
 
-// FAQ
-const faqs = document.querySelectorAll(".faq");
+// thumbnails
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const des = document.getElementById("des");
 
-faqs.forEach((faq) => {
-    faq.addEventListener("click", () => {
-        faq.classList.toggle("active");
-    });
+one.addEventListener("mousemove", function()
+{
+    des.innerHTML = "Meningkatkan pemahaman masyarakat tentang <br> bullying dengan meningkatkan pemahaman <br> masyarakat , diharapkan masyarakat dapat lebih <br> memahami masalah bullying dan mengambil <br> tindakan yang tepat untuk mencegah <br> dan mengatasinya"
+});
+two.addEventListener("mousemove", function()
+{
+    des.innerHTML = "Mendorong masyarakat untuk mengambil tindakan <br>untuk mencegah bulliying. Dengan mendorong <br>
+    masyarakat untuk mengambil tindakan, diharapkan <br>
+    dapat menciptakan lingkungan yang aman dan <br> nyaman bagi semua orang"
+});
+three.addEventListener("mousemove", function()
+{
+    des.innerHTML = "hello3"
+});
+four.addEventListener("mousemove", function()
+{
+    des.innerHTML = "hello4"
 });
